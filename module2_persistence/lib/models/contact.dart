35486c821dc4fb7f03f223efbca4830eb4b5de3d
Leonardo <leomelniski2@gmail.com>
2019-12-03 07:09:56 -0300

    model of contact

---
+++ module2_persistence/lib/models/contact.dart
@@ -0,0 +1,13 @@
+
+class Contact {
+  final int id;
+  final String name;
+  final int accNumber;
+
+
+  Contact(this.id, this.name, this.accNumber);
+
+  @override
+  String toString() => 'Contact{$id, $name, $accNumber}';
+
+}
